a162a16a7e2390b89ac4fbd11a2caab4be47c2b1
Riaz Virani <riaz.n.virani@gmail.com>
2021-04-11 17:25:50 -0400

    Remove needless extend complexity

--- lib/light-service/action.rb
+++ lib/light-service/action.rb
@@ -1,53 +1,45 @@
-require 'active_support/deprecation'
-
 module LightService
   module Action
-    def self.extended(base_class)
-      base_class.extend Macros
+    def expects(*args)
+      expected_keys.concat(args)
     end
 
-    module Macros
-      def expects(*args)
-        expected_keys.concat(args)
-      end
+    def promises(*args)
+      promised_keys.concat(args)
+    end
 
-      def promises(*args)
-        promised_keys.concat(args)
-      end
+    def expected_keys
+      @expected_keys ||= []
+    end
 
-      def expected_keys
-        @expected_keys ||= []
-      end
+    def promised_keys
+      @promised_keys ||= []
+    end
 
-      def promised_keys
-        @promised_keys ||= []
-      end
+    def executed
+      define_singleton_method :execute do |context = {}|
+        action_context = create_action_context(context)
+        return action_context if action_context.stop_processing?
 
-      def executed
-        define_singleton_method :execute do |context = {}|
-          action_context = create_action_context(context)
-          return action_context if action_context.stop_processing?
+        # Store the action within the context
+        action_context.current_action = self
 
-          # Store the action within the context
-          action_context.current_action = self
+        Context::KeyVerifier.verify_keys(action_context, self) do
+          action_context.define_accessor_methods_for_keys(expected_keys + promised_keys)
 
-          Context::KeyVerifier.verify_keys(action_context, self) do
-            action_context.define_accessor_methods_for_keys(expected_keys + promised_keys)
-
-            catch(:jump_when_failed) do
-              yield(action_context)
-            end
+          catch(:jump_when_failed) do
+            yield(action_context)
           end
         end
       end
+    end
 
-      private
+    private
 
-      def create_action_context(context)
-        return context if context.is_a? LightService::Context
+    def create_action_context(context)
+      return context if context.is_a? LightService::Context
 
-        LightService::Context.make(context)
-      end
+      LightService::Context.make(context)
     end
   end
 end
--- lib/light-service/organizer.rb
+++ lib/light-service/organizer.rb
@@ -1,46 +1,36 @@
 module LightService
   module Organizer
-    def self.extended(base_class)
-      base_class.extend ClassMethods
-      base_class.extend Macros
+    def with(data = {})
+      data[:_aliases] = @aliases if @aliases
+
+      WithReducer.new(self).with(data)
     end
 
-    # In case this module is included
-    module ClassMethods
-      def with(data = {})
-        data[:_aliases] = @aliases if @aliases
+    def reduce(*actions)
+      with({}).reduce(actions)
+    end
 
-        WithReducer.new(self).with(data)
-      end
+    def execute(code_block)
+      lambda do |ctx|
+        return ctx if ctx.stop_processing?
 
-      def reduce(*actions)
-        with({}).reduce(actions)
-      end
-
-      def execute(code_block)
-        lambda do |ctx|
-          return ctx if ctx.stop_processing?
-
-          code_block.call(ctx)
-          ctx
-        end
-      end
-
-      def add_to_context(**args)
-        args.map do |key, value|
-          execute(->(ctx) { ctx[key.to_sym] = value })
-        end
-      end
-
-      def add_aliases(args)
-        execute(->(ctx) { ctx.assign_aliases(ctx.aliases.merge(args)) })
+        code_block.call(ctx)
+        ctx
       end
     end
 
-    module Macros
-      def aliases(key_hash)
-        @aliases = key_hash
+    def add_to_context(**args)
+      args.map do |key, value|
+        execute(->(ctx) { ctx[key.to_sym] = value })
       end
+    end
+
+    def add_aliases(args)
+      execute(->(ctx) { ctx.assign_aliases(ctx.aliases.merge(args)) })
+    end
+
+    def aliases(key_hash)
+      @aliases = key_hash
     end
   end
 end
